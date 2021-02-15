34c5eb28e380a98c1e26aecc01481212914c67a1
Patrik Horemans <horemansp@hotmail.com>
2021-02-15 16:43:42 +0100

    Delete sma_API.py

--- sma_API.py
+++
@@ -1,8 +0,0 @@
-from sma_sunnyboy import *
-import urllib3
-urllib3.disable_warnings()
-client = WebConnect("192.168.0.237", Right.USER, "Soloya2019", True)
-client.auth()
-pow_current = client.get_value(Key.pow_current)
-print("real time power=",pow_current)
-client.logout()
